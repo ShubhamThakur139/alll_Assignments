a695d6b2047e5102a70b348ab3714e5a7c9a83e5
Shubham Thakur <shubhamthakur2920@gmail.com>
2021-08-18 00:26:41 +0530

    new Assignment

--- app.js
+++ app.js
@@ -1,4 +1,30 @@
-var arr = [1, 2, 3, 4, 5, 6];
-console.log(arr[2]);
-// var arr = [{ name: "Shubham" }, { name: "Ankush" }, { name: "Kanav" }];
-// console.log(arr[1]);
+// var arr = [1, 2, 3, 4, 5, 6];
+// var arr2 = [7, 8, 9, 10];
+// var cparr = [...arr, ...arr2];
+// cparr[0] = 10;
+// console.log(arr);
+// console.log(cparr);
+
+// var person = {
+//   name: "shubham",
+//   age: 21,
+// };
+
+// var person1 = { ...person };
+// person1.name = "Shubham Thakur";
+// console.log(person);
+// console.log(person1);
+
+// var ar = [1, 2, 3, 4, 5, 6];
+
+// var temparr = [];
+// for (var i = 0; i < ar.length; i++) {
+//   if (i == 2) {
+//     continue;
+//   } else {
+//     temparr[i] = ar[i];
+//   }
+// }
+
+// console.log(ar);
+// console.log(temparr);
